84c5e78b98e166fa5adfaa294ec8a395aabb7d48
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-04-12 03:09:45 +0530

    Added form to the landing page

---
+++ src/scripts/js/form.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    // Switch to Register
+    $('.needAccount, .backLogin').click(function() {
+        $('#login, #register, #formContainer').toggleClass('switch');
+    });
+
+    // Open Forgot Password
+    $('.forgotBtn, .backLoginF').click(function() {
+        $('#forgot').toggleClass('forgot');
+    });
+
+
+});
