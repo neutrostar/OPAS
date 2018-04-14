55825aa5f61085682804c5695a8ac86869ba4992
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-04-14 20:59:06 +0530

    Added new navbar and other feautures

--- main.js
+++ main.js
@@ -4,19 +4,23 @@ const path = require('path');
 
 const {app, BrowserWindow} = electron;
 
-let mainWindow
+let mainWindow;
 
 app.on('ready', function(){
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
-
+        height: 600,
+        frame: false,
+        backgroundColor: "#ff00aa"
     });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
+    if(!mainWindow.isFullScreen()){
+        mainWindow.setFullScreen(true);
+    }
     // mainWindow.webContents.on('did-finish-load', function() {
     //     mainWindow.show();
     // });
