e27d01cc5bfd7751158e85349a6550b05f99882f
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-05-09 17:43:37 +0530

    Added Splash window

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let splashWindow;
 let addWindow;
 
+
+
 app.on('ready', function(){
+    MakeSplashWindow();
+    //splashWindow.close();
     mainWindow = new BrowserWindow({
         frame: false,
         minHeight:720,
@@ -36,7 +41,6 @@ app.on('ready', function(){
 
 const mainMenuTemplate = [
     {
-
     // help menu
     label: 'Help',
     submenu:[
@@ -75,4 +79,18 @@ const mainMenuTemplate = [
 
 if(process.platform == 'darwin'){
     mainMenuTemplate.unshift({});
+}
+
+function MakeSplashWindow(){
+    splashWindow = new BrowserWindow(({
+        frame: false,
+        height: 300,
+        width: 400
+    }));
+    splashWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
 }
---
+++ src/scripts/js/faculty_links.js
@@ -0,0 +1,8 @@
+const electron = require('electron');
+const {remote} = electron;
+const {BrowserWindow, app} = remote;
+let QuizWindow;
+
+function CreateQuizWindow(){
+    QuizWindow = new QuizWindow
+}
