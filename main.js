4c2a9f7581114fa673a5440f2fbffa0342e5c25c
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-05-09 21:33:30 +0530

    Fixed main

--- main.js
+++ main.js
@@ -4,20 +4,19 @@ const path = require('path');
 
 const {app, BrowserWindow, Menu} = electron;
 
+
+process.env.NODE_ENV = 'Development';
+
 let mainWindow;
-let splashWindow;
 let addWindow;
 
-
-
 app.on('ready', function(){
-    MakeSplashWindow();
-    //splashWindow.close();
     mainWindow = new BrowserWindow({
         frame: false,
         minHeight:720,
         minWidth:1280,
-        resizable: false
+        resizable: false,
+        preload: __dirname+'/hello.html'
     });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
@@ -37,183 +36,138 @@ app.on('ready', function(){
         const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
         Menu.setApplicationMenu(mainMenu);
     }
+
+    mainWindow.getNativeWindowHandle()
 });
 
 const mainMenuTemplate = [
     {
 
-<<<<<<< HEAD
-    // help menu
-    label: 'Help',
-    submenu:[
-
-
       // file menu
       label: 'File',
       submenu:[
-=======
-    // file menu
-    label: 'File',
-    submenu:[
->>>>>>> parent of f1ee009... Activated assignment link in student module
 
-      {
-        label:'Contact Us',
-        click(){
-          //file yet to be added
-          ;
+        {
+          label:'Preferences',
+          click(){
+            //file yet to be added
+            ;
+          }
+        },
+        {
+          label: 'Quit',
+          accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+          click(){
+            app.quit();
+          }
         }
-      },
-      {
-        label:'About',
-        click(){
-          // file yet to be added
-          ;
+      ]
+    },
+
+
+
+
+    {
+        // Edit menu
+      label: 'Edit',
+      submenu:[
+
+        {
+          label:'Cut',
+          click(){
+            //file yet to be added
+            ;
+          }
+        },
+        {
+          label:'Copy',
+          click(){
+            // file yet to be added
+            ;
+          }
+        },
+        {
+          label: 'Paste',
+          click(){
+            // file yet to be added
+            ;
+          }
+        },
+      ]
+    },
+    
+
+
+
+
+    {
+      // View menu
+      label: 'View',
+      submenu:[
+
+        {
+          label:'Student Module',
+          click(){
+            //file yet to be added
+            ;
+          }
+        },
+        {
+          label:'Teacher Module',
+          click(){
+            // file yet to be added
+            ;
+          }
+        },
+      ]
+
+    },
+
+    {
+
+      // Help menu
+      label: 'Help',
+      submenu:[
+
+        {
+          label:'Contact Us',
+          click(){
+            //file yet to be added
+            ;
+          }
+        },
+        {
+          label:'About',
+          click(){
+            // file yet to be added
+            ;
+          }
+        },
+        {
+          label: 'Updates',
+          click(){
+            // file yet to be added
+            ;
+          }
+        },
+        {
+          label: 'Quit',
+          accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+          click(){
+            app.quit();
+          }
         }
-      },
-      {
-        label: 'Updates',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
-          app.quit();
-        }
-      }
-    ]
+      ]
 
 
 
 
+    }
+];
 
-    // Edit menu
-    label: 'Edit',
-    submenu:[
-
-      {
-        label:'Contact Us',
-        click(){
-          //file yet to be added
-          ;
-        }
-      },
-      {
-        label:'About',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Updates',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
-          app.quit();
-        }
-      }
-    ]
-
-
-
-
-
-    // View menu
-    label: 'View',
-    submenu:[
-
-      {
-        label:'Contact Us',
-        click(){
-          //file yet to be added
-          ;
-        }
-      },
-      {
-        label:'About',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Updates',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
-          app.quit();
-        }
-      }
-    ]
-
-
-
-    // Help menu
-    label: 'Help',
-    submenu:[
-
-      {
-        label:'Contact Us',
-        click(){
-          //file yet to be added
-          ;
-        }
-      },
-      {
-        label:'About',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Updates',
-        click(){
-          // file yet to be added
-          ;
-        }
-      },
-      {
-        label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
-          app.quit();
-        }
-      }
-    ]
-
-
-
-
-<<<<<<< HEAD
-function MakeSplashWindow(){
-    splashWindow = new BrowserWindow(({
-        frame: false,
-        height: 300,
-        width: 400
-    }));
-    splashWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file',
-        slashes: true
-    }));
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
 
 
 if(process.env.NODE_ENV !== 'production'){
@@ -235,12 +189,4 @@ if(process.env.NODE_ENV !== 'production'){
     }
   );
 
-
-=======
-  }
-];
-
-if(process.platform == 'darwin'){
-    mainMenuTemplate.unshift({});
->>>>>>> parent of f1ee009... Activated assignment link in student module
 }
