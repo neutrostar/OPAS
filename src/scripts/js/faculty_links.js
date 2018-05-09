af5eecd26281a8c930b024e66f8b9ee4791c398a
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-05-09 20:12:52 +0530

    Revert "Added Splash window"
    This reverts commit e27d01cc5bfd7751158e85349a6550b05f99882f.

--- src/scripts/js/faculty_links.js
+++
@@ -1,8 +0,0 @@
-const electron = require('electron');
-const {remote} = electron;
-const {BrowserWindow, app} = remote;
-let QuizWindow;
-
-function CreateQuizWindow(){
-    QuizWindow = new QuizWindow
-}
