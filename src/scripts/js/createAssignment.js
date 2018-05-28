7427c4c80aa3918e0542aaa175392351fef32f5e
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-05-28 18:37:25 +0530

    Added Assignment and Group Facilities for the faculty user group

---
+++ src/scripts/js/createAssignment.js
@@ -0,0 +1,46 @@
+const electron = require('electron');
+const {remote} = electron;
+const {BrowserWindow, app} = remote;
+
+let NewAssignmentWindow;
+let ViewAssignmentWIndow
+
+function CreateAssignmentWindow(){
+    NewAssignmentWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "About-OPAS",
+        frame: false,
+        parent: remote.getCurrentWindow(),
+        modal: true,
+        show: false
+    });
+    var theUrl = 'file://' + __dirname + '/faculty_createNewAssignment.html';
+    NewAssignmentWindow.loadURL(theUrl);
+    NewAssignmentWindow.once('ready-to-show', ()=>{
+        NewAssignmentWindow.show();
+    });
+    NewAssignmentWindow.on('closed', function () {
+        NewAssignmentWindow = null;
+    });
+}
+
+function CreateAssignmentView(){
+    ViewAssignmentWIndow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "About-OPAS",
+        frame: false,
+        parent: remote.getCurrentWindow(),
+        modal: true,
+        show: false
+    });
+    var theUrl = 'file://' + __dirname + '/faculty_assignment_viewsubmissions.html';
+    ViewAssignmentWIndow.loadURL(theUrl);
+    ViewAssignmentWIndow.once('ready-to-show', ()=>{
+        ViewAssignmentWIndow.show();
+    });
+    ViewAssignmentWIndow.on('closed', function () {
+        ViewAssignmentWIndow = null;
+    });
+}
