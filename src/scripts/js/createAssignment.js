3be4220c3c9e2d58736c184641cea496fd4f6819
Apoorv Shrivastav <apoorvshrivastav.1@gmail.com>
2018-05-30 14:12:56 +0530

    Bug Fixes here and there

--- src/scripts/js/createAssignment.js
+++ src/scripts/js/createAssignment.js
@@ -1,5 +1,7 @@
 let NewAssignmentWindow;
-let ViewAssignmentWIndow
+let ViewAssignmentWIndow;
+let EnterCodeWindow;
+let EnterStudentIDWindow;
 
 function CreateAssignmentWindow(){
     NewAssignmentWindow = new BrowserWindow({
@@ -38,5 +40,44 @@ function CreateAssignmentView(){
     });
     ViewAssignmentWIndow.on('closed', function () {
         ViewAssignmentWIndow = null;
+    });
+}
+
+function EnterGroupCode(){
+    EnterCodeWindow = new BrowserWindow({
+        width: 400,
+        height: 200,
+        title: "About-OPAS",
+        frame: false,
+        parent: remote.getCurrentWindow(),
+        modal: true,
+        show: false
+    });
+    var theUrl = 'file://' + __dirname + '/student_entercode.html';
+    EnterCodeWindow.loadURL(theUrl);
+    EnterCodeWindow.once('ready-to-show', ()=>{
+        EnterCodeWindow.show();
+    });
+    EnterCodeWindow.on('closed', function () {
+        EnterCodeWindow = null;
+    });
+}
+function EnterStudentID(){
+    EnterStudentIDWindow = new BrowserWindow({
+        width: 400,
+        height: 200,
+        title: "About-OPAS",
+        frame: false,
+        parent: remote.getCurrentWindow(),
+        modal: true,
+        show: false
+    });
+    var theUrl = 'file://' + __dirname + '/faculty_addstudent.html';
+    EnterStudentIDWindow.loadURL(theUrl);
+    EnterStudentIDWindow.once('ready-to-show', ()=>{
+        EnterStudentIDWindow.show();
+    });
+    EnterStudentIDWindow.on('closed', function () {
+        EnterStudentIDWindow = null;
     });
 }
