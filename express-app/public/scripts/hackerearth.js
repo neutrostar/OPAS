eb4108f7576c2c2cb94f7bb0d2728929355f633f
sidnevski <sidsrivast42@gmail.com>
2018-11-07 19:02:21 +0530

    Minor changes

--- express-app/public/scripts/hackerearth.js
+++ express-app/public/scripts/hackerearth.js
@@ -1,3 +1,3 @@
 var HackerEarth = require('hackerEarth-node');
 
-var hackerEarth = new HackerEarth("472192bc9c58c8e3dff61c2c3ad71e718abd18e1");
+const hackerEarth = new HackerEarth("472192bc9c58c8e3dff61c2c3ad71e718abd18e1");
