03cb591e740e6ddc4cd91a21047b941c9a84eba3
sidnevski <sidsrivast42@gmail.com>
2018-11-09 21:41:44 +0530

    Minor changes, submission model created

--- express-app/models/submission.js
+++ express-app/models/submission.js
@@ -3,6 +3,17 @@ var mongoose = require("mongoose");
 // Schema setup
 var SubmissionSchema = new mongoose.Schema({
 
+	user: {
+
+		id: {
+
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "user"
+		},
+
+		username: String
+	},
+	
 	assignment: {
 
 		id: {
--- express-app/public/scripts/hackerearth.js
+++ express-app/public/scripts/hackerearth.js
@@ -1,3 +1,3 @@
-var HackerEarth = require('hackerEarth-node');
+var HackerEarth = require("hackerEarth-node");
 
 const hackerEarth = new HackerEarth("472192bc9c58c8e3dff61c2c3ad71e718abd18e1");
